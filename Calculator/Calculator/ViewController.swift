2e307d2a9fe2f30beaf237a70135220349d8e326
jakouk <jakoriaty@gmail.com>
2016-09-14 21:27:15 +0900

    edit ViewController.swift

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,13 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleofTyping = false
     
     @IBAction func touchDigit(sender: UIButton){
         let digit = sender.currentTitle!
-        print("toched \(digit) digit")
         
+        if userIsInTheMiddleofTyping{
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        }else{
+            display.text = digit
+        }
+        userIsInTheMiddleofTyping = true
     }
     
+    @IBAction func performOperation(sender: UIButton) {
+        userIsInTheMiddleofTyping = false
+        if let mathematicalSymbol = sender.currentTitle{
+            if mathematicalSymbol == "π" {
+                display.text = String(M_PI)
+            }
+        }
+    }
     
 }
 
